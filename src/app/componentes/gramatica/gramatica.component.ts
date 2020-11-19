f498808cbf936f7d44c31a0f9881c74b0f977e79
Saulo Poveda Montesdeoca <saulopm.1989@gmail.com>
2020-11-19 03:37:51 +0000

    [ Gramática ] » Actualizar contenido completado

---
+++ src/app/componentes/gramatica/gramatica.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+// Get parameter from URL
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-gramatica',
+  templateUrl: './gramatica.component.html'
+})
+export class GramaticaComponent implements OnInit {
+
+  titulo      : string;
+  tiempoVerbal: string;
+
+  constructor( private activatedRoute: ActivatedRoute ) { }
+
+  ngOnInit() {
+    this.getTiempoVerbal();
+  }
+
+  // ──────────────── //
+  //     AUXILIAR     //
+  // ──────────────── //
+
+  getTiempoVerbal() {
+    this.activatedRoute.params.subscribe( parametroURL => {
+      this.tiempoVerbal = parametroURL.id;
+      this.titulo = this.sanitize( this.tiempoVerbal );
+    });
+  }
+
+  sanitize( secuencia: string ) {
+
+    // Cambiamos los guiones altos por espacios
+    secuencia = secuencia.replace( /-/g, ' ' );
+
+    // Pasamos la primera letra a mayúscula
+    secuencia = secuencia.replace( secuencia[0], secuencia[0].toUpperCase() );
+    
+    return secuencia;
+  }
+}
