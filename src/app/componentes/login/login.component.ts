f5b6b28609e29a226cb9720cb0cbf368fa9c57a4
Saulo Poveda Montesdeoca <saulopm.1989@gmail.com>
2020-11-21 03:53:24 +0000

    [ Login ] » Separar los estilos del login en una hoja de estilos aparte completado

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -18,7 +18,8 @@ declare var $: any;
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
